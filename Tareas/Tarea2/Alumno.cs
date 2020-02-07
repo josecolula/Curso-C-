2180fd38c13218a3b422589ce250f4a5ede259ca
josecolula <jose.colula@bec.seguridad.unam.mx>
2020-02-07 02:17:42 -0600

    Tarea 2

---
+++ Tareas/Tarea2/Alumno.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tarea2
+{
+    class Alumno
+    {
+        string nombre;
+        string ap_p;
+        string ap_m;
+        int cal_proyecto;
+        int cant_tareas;
+        int cant_part;
+        int cal_final;
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public string ApellidoPaterno
+        {
+            get { return ap_p; }
+            set { ap_p = value; }
+        }
+        public string ApellidoMaterno
+        {
+            get { return ap_m; }
+            set { ap_m = value; }
+        }
+        public int CalificacionProyecto
+        {
+            get { return cal_proyecto; }
+            set
+            {
+                if(value < 5)
+                {
+                    cal_proyecto = 5;
+                }
+                else if(value > 10)
+                {
+                    cal_proyecto = 10;
+                }
+                else
+                {
+                    cal_proyecto = value;
+                }
+            }
+        }
+        public int CantidadTareas
+        {
+            get { return cant_tareas; }
+            set
+            {
+                if(value < 0)
+                {
+                    cant_tareas = 0;
+                }
+                else if(value > 5)
+                {
+                    cant_tareas = 5;
+                }
+                else
+                {
+                    cant_tareas = value;
+                }
+            }
+        }
+        public int CantidadParticipaciones
+        {
+            get { return cant_part; }
+            set
+            {
+                if(value < 0)
+                {
+                    cant_part = 0;
+                }
+                else
+                {
+                    cant_part = value;
+                }
+            }
+        }
+        public int CalificacionFinal
+        {
+            get { return cal_final; }
+            set
+            {
+                if(value > 100)
+                {
+                    cal_final = 100;
+                }
+                else if (value < 50)
+                {
+                    cal_final = 50;
+                }
+                else
+                {
+                    cal_final = value;
+                }
+            }
+        }
+        public Alumno() { }
+        public override string ToString()
+        {
+            return string.Format("Bienvenido a la Calificadora de Alumnos");
+        }
+
+    }
+}
---
+++ Tareas/Tarea2/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+namespace Tarea2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Alumno a = new Alumno();
+            Console.WriteLine(a.ToString());
+            Console.WriteLine();
+            Console.WriteLine("Ingresa el número de alumnos a calificar: ");
+            int tam = Convert.ToInt32(Console.ReadLine());
+            Alumno[] alumnos = new Alumno[tam];
+            Console.WriteLine();
+            for(int i=0; i<alumnos.Length; i++)
+            {
+                Alumno objalum = new Alumno();
+                Console.WriteLine("Datos del alumno {0}", i + 1);
+                Console.WriteLine("Nombre: ");
+                objalum.Nombre = Console.ReadLine();
+                //alumnos[i].Nombre = objalum.Nombre;
+                Console.WriteLine("Apellido Paterno: ");
+                objalum.ApellidoPaterno = Console.ReadLine();
+                //alumnos[i].ApellidoPaterno = objalum.ApellidoPaterno;
+                Console.WriteLine("Apellido Materno: ");
+                objalum.ApellidoMaterno = Console.ReadLine();
+                //alumnos[i].ApellidoMaterno = objalum.ApellidoMaterno;
+                Console.WriteLine("Calificación del Proyecto: ");
+                objalum.CalificacionProyecto = Convert.ToInt32(Console.ReadLine());
+                //alumnos[i].CalificacionProyecto = objalum.CalificacionProyecto;
+                Console.WriteLine("Cantidad de Tareas: ");
+                objalum.CantidadTareas = Convert.ToInt32(Console.ReadLine());
+                //alumnos[i].CantidadTareas = objalum.CantidadTareas;
+                Console.WriteLine("Cantidad de Participaciones: ");
+                objalum.CantidadParticipaciones = Convert.ToInt32(Console.ReadLine());
+                //alumnos[i].CantidadParticipaciones = objalum.CantidadParticipaciones;
+                objalum.CalificacionFinal = 50;
+                //alumnos[i].CalificacionFinal = objalum.CalificacionFinal;
+                alumnos[i] = objalum;
+                Console.WriteLine();
+            }
+            Calculadora(alumnos);
+            Console.ReadKey();
+        }
+        static void Calculadora(Alumno[] alumnos)
+        {
+            int calificacion_f = 0;
+            int tareas=0;
+            int i = 0;
+            for(int k=0; k < alumnos.Length; k++)
+            {
+                calificacion_f = 0;
+                tareas = 0;
+                calificacion_f = (alumnos[k].CalificacionProyecto * 6);
+                for( int j=0; j< alumnos[k].CantidadTareas; j++)
+                {
+                    tareas = tareas + 10;
+                }
+                calificacion_f = calificacion_f + ((tareas / 5) * 4);
+                if(alumnos[k].CantidadParticipaciones > 5)
+                {
+                    calificacion_f = calificacion_f + 8;
+                }
+
+                alumnos[k].CalificacionFinal = calificacion_f; 
+            }
+            foreach(Alumno objalu3 in alumnos)
+            {
+                Console.WriteLine("La calificación final de " + objalu3.Nombre +" "+ objalu3.ApellidoPaterno+" "+objalu3.ApellidoMaterno+" es de: " + objalu3.CalificacionFinal+"/100");
+
+            }
+        }
+    }
+}
